08d5b1f60e991e9ad8ba2949b6f19c3c6cd11245
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-10 09:35:10 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+In this excercise you will add two numbers together. 
+
+1) Declare two varaibles for 'x' and 'y' - set the initial values for each to any number between 1 and 500.
+
+2) Create a function 'addNumbers' with two parameters called 'num1' and 'num2'. Name the function whatever you'd like. 
+
+3) Call the 'addNumbers' function and use X and Y as the parameter values - e.g. (x, y) 
+
+4) Log the result to the console from within the 'addNumbers' function.
+
+NOTE: For step 3, you can do one of two things:
+
+1) Declare a variable in the 'addNumbers' function for the 'answer', set answer to equal 'num1 + num2', and log it to the console.
+2) Or, use 'console.log(num1 + num2)'
+
+*/
